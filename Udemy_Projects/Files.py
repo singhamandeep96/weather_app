4061fb4fda97a43eb01cd2b36bb2aae1620de381
Amandeep Singh <singhamandeep96>
2026-03-10 03:06:17 +0000

    Keep only weather_app.py

--- Udemy_Projects/Files.py
+++
@@ -1,4 +0,0 @@
-with open("my_file.txt") as file:
-    contents= file.read()
-    print(contents)
-
--- Udemy_Projects/Tip_calculator.py
+++
@@ -1,9 +0,0 @@
-bill=float(input("What was the total bill ?"))
-tip=int(input("What percentage of tip would you like to give ? 10 12 15"))
-people=int(input("How many people are splitting the bill ?"))
-tip_as_percent= tip/100
-total_tip= bill*tip_as_percent
-total_bill=bill+total_tip
-bill_per_person=total_bill/people
-final_amount=round(bill_per_person, 2)
-print(f"Each Person should pay: {final_amount}")
